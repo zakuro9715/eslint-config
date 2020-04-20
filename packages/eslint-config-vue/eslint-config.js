57ad89801829995877029ccb4f827326ac095920
z@kuro <z@kuro.red>
2020-04-20 12:08:55 +0900

    Add rules to vue

--- packages/eslint-config-vue/eslint-config.js
+++ packages/eslint-config-vue/eslint-config.js
@@ -1,3 +1,4 @@
+const INLINE_ELEMENTS = require('eslint-plugin-vue/lib/utils/inline-non-void-elements.json')
 module.exports = {
   extends: [
     'plugin:vue/essential',
@@ -6,4 +7,9 @@ module.exports = {
     '@vue/standard',
     '@pianoforte',
   ],
+  rules: {
+    'vue/singleline-html-element-content-newline': ['error', {
+      ignores: ['pre', 'textarea', 'nuxt-link', 'router-link'].concat(INLINE_ELEMENTS),
+    }],
+  },
 }
