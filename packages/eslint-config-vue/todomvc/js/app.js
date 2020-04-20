97caae515b813b20ed16dc15f2e096bb168f2857
z@kuro <z@kuro.red>
2020-04-20 12:02:51 +0900

    Lintfix

--- packages/eslint-config-vue/todomvc/js/app.js
+++ packages/eslint-config-vue/todomvc/js/app.js
@@ -20,20 +20,23 @@
     // the root element that will be compiled
     el: '.todoapp',
 
+    // a custom directive to wait for the DOM to be updated
+    // before focusing on the input field.
+    // http://vuejs.org/guide/custom-directive.html
+    directives: {
+      'todo-focus': function (el, binding) {
+        if (binding.value) {
+          el.focus()
+        }
+      },
+    },
+
     // app initial state
     data: {
       todos: todoStorage.fetch(),
       newTodo: '',
       editedTodo: null,
       visibility: 'all',
-    },
-
-    // watch todos change for localStorage persistence
-    watch: {
-      todos: {
-        deep: true,
-        handler: todoStorage.save,
-      },
     },
 
     // computed properties
@@ -54,6 +57,14 @@
             todo.completed = value
           })
         },
+      },
+    },
+
+    // watch todos change for localStorage persistence
+    watch: {
+      todos: {
+        deep: true,
+        handler: todoStorage.save,
       },
     },
 
@@ -102,17 +113,6 @@
 
       removeCompleted: function () {
         this.todos = filters.active(this.todos)
-      },
-    },
-
-    // a custom directive to wait for the DOM to be updated
-    // before focusing on the input field.
-    // http://vuejs.org/guide/custom-directive.html
-    directives: {
-      'todo-focus': function (el, binding) {
-        if (binding.value) {
-          el.focus()
-        }
       },
     },
   })
