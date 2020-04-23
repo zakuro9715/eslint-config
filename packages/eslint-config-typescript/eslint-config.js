1a202020bcd3dde795434aca07c9b3e62f39fd3b
z@kuro <z@kuro.red>
2020-04-23 16:12:36 +0900

    [eslint-config-typescript] Create project

---
+++ packages/eslint-config-typescript/eslint-config.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@pianoforte',
+  ],
+  plugins: [
+    '@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  rules: {
+  },
+}
