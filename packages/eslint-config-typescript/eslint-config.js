5d8fcc7c946eff07f26298ab082b8d23a8fb0b5c
z@kuro <z@kuro.red>
2020-04-24 01:13:57 +0900

    [eslint-config-typescript] Add rules

--- packages/eslint-config-typescript/eslint-config.js
+++ packages/eslint-config-typescript/eslint-config.js
@@ -1,4 +1,31 @@
-/* eslint-env node */
+const namingConventions = [
+  {
+    selector: 'default',
+    format: ['camelCase'],
+  },
+  {
+    selector: 'variable',
+    format: ['camelCase', 'UPPER_CASE'],
+    leadingUnderscore: 'allow',
+  },
+  {
+    selector: 'variableLike',
+    format: ['camelCase'],
+    leadingUnderscore: 'allow',
+  },
+  {
+    selector: 'memberLike',
+    modifiers: ['private'],
+    format: ['camelCase'],
+    leadingUnderscore: 'allow',
+  },
+  {
+    selector: 'typeLike',
+    format: ['PascalCase'],
+  },
+]
+
+
 module.exports = {
   extends: [
     'eslint:recommended',
@@ -12,10 +39,39 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      multiline: {
+        delimiter: 'none',
+        requireLast: true,
+      },
+    }],
+    '@typescript-eslint/consistent-type-assertions': ['error', {
+      assertionStyle: 'as',
+      objectLiteralTypeAssertions: 'never',
+    }],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/explicit-member-accessibility': ['error', {
+      accessibility: 'no-public',
+    }],
+    '@typescript-eslint/method-signature-style': ['error', 'property'],
+    '@typescript-eslint/naming-convention': ['error', ...namingConventions],
+    '@typescript-eslint/no-explicit-any': 'off',
     'no-dupe-class-members': 'off',
     '@typescript-eslint/no-dupe-class-members': 'error',
-    '@typescript-eslint/no-explicit-any': 'off',
     'no-useless-constructor': 'off',
     '@typescript-eslint/no-useless-constructor': 'error',
+    '@typescript-eslint/no-for-in-array': 'error',
+    '@typescript-eslint/no-implied-eval': 'error',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
+    '@typescript-eslint/no-throw-literal': 'error',
+    '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
+    '@typescript-eslint/prefer-for-of': 'error',
+    '@typescript-eslint/prefer-nullish-coalescing': 'error',
+    '@typescript-eslint/promise-function-async': 'error',
+    '@typescript-eslint/restrict-plus-operands': 'error',
+    '@typescript-eslint/restrict-template-expressions': ['error', {
+      allowNumber: true,
+      allowBoolean: true,
+    }],
   },
 }
