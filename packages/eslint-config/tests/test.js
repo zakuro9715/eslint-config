53fdce4a7bf9f89ee11ac825e4f8259ff9b9fb6d
z@kuro <z@kuro.red>
2020-07-27 06:32:19 +0900

    Purge tests

--- packages/eslint-config/tests/test.js
+++
@@ -1,5 +0,0 @@
-const path = require('path')
-const config = require('../eslint-config')
-const { testRules } = require('../../../test-helper')
-
-testRules(config, path.join(__dirname, 'rules', './**/*.js'))
