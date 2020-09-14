8f719cf5c441d47ddac4f8e2675ab9fd7d853359
zakuro <z@kuro.red>
2020-09-14 21:55:48 +0900

    Add config for vue-next

---
+++ packages/eslint-config-vue-typescript/next.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: [
+    './index.js',
+    '@zakuro9715/vue/next',
+  ],
+}
---
+++ packages/eslint-config-vue/next.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    './index.js',
+    'plugin:vue/vue3-recommended',
+  ],
+  rules: {
+    'vue/no-multiple-template-root': 'off',
+  },
+}
