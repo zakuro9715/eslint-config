c1ca70026033a2bad1619e87a1f620a9bd2536fc
zakuro <z@kuro.red>
2021-01-30 16:52:11 +0900

    format

--- packages/eslint-config-vue-typescript/index.js
+++ packages/eslint-config-vue-typescript/index.js
@@ -5,25 +5,27 @@ module.exports = {
     '@zakuro9715',
     '@zakuro9715/eslint-config-typescript',
   ],
-  overrides: [{
-    files: ['**/*.vue'],
-    extends: [
-      '@vue/typescript',
-      '@zakuro9715/vue',
-      '@zakuro9715/eslint-config-typescript/ts',
-    ],
-    parser: 'vue-eslint-parser',
-    parserOptions: {
-      parser: '@typescript-eslint/parser',
-      project: 'tsconfig.eslint.json',
+  overrides: [
+    {
+      files: ['**/*.vue'],
+      extends: [
+        '@vue/typescript',
+        '@zakuro9715/vue',
+        '@zakuro9715/eslint-config-typescript/ts',
+      ],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        project: 'tsconfig.eslint.json',
+      },
+      rules: {
+        '@typescript-eslint/naming-convention': ['error', ...namingConventions, {
+          selector: 'property',
+          format: ['camelCase', 'PascalCase'], // For components
+        }],
+        '@typescript-eslint/restrict-template-expressions': 'off',
+        'vue/require-direct-export': 'off', // https://github.com/vuejs/eslint-plugin-vue/issues/907
+      },
     },
-    rules: {
-      '@typescript-eslint/naming-convention': ['error', ...namingConventions, {
-        selector: 'property',
-        format: ['camelCase', 'PascalCase'], // For components
-      }],
-      '@typescript-eslint/restrict-template-expressions': 'off',
-      'vue/require-direct-export': 'off', // https://github.com/vuejs/eslint-plugin-vue/issues/907
-    },
-  }],
+  ],
 }
